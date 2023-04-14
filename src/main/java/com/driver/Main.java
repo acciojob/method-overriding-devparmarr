56265a1be8760498a946a2c3461d3ea1025a5e2e
devparmarr <devparmarr123@gmail.com>
2023-04-15 03:30:45 +0530

    Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,19 @@ package com.driver;
 
 public class Main {
   
+    B objB=new B();
+    String result= objB.meth();
+}
+
+class A{
+    public String meth(){
+    
+        return "Invoking method from class A";
+    }
+}
+
+class B extends A{
+    public String meth() {
+        return "Method is overridden in Extended class B";
+     }
 }
